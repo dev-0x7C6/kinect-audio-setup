5ce56f2a88df03c9560eef61c7d6532cd48ff5d9
Antonio Ospite <ao2@ao2.it>
2016-01-05 20:08:18 +0100

    kinect_upload_fw: initialize current_configuration before libusb_get_configuration()
    Initialize the variable as suggested by http://libusb.sourceforge.net/api-1.0/caveats.html

--- kinect_upload_fw/kinect_upload_fw.c
+++ kinect_upload_fw/kinect_upload_fw.c
@@ -155,13 +155,14 @@ int main(int argc, char** argv) {
 		goto fail_libusb_open;
 	}
 
-	int current_configuration = 0;
+	int current_configuration = -1;
 	libusb_get_configuration(dev, &current_configuration);
 	if (current_configuration != 1)
 		libusb_set_configuration(dev, 1);
 
 	libusb_claim_interface(dev, 0);
 
+	current_configuration = -1;
 	libusb_get_configuration(dev, &current_configuration);
 	if (current_configuration != 1) {
 		res = -ENODEV;
